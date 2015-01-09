5ab73ba96de64ac6c3f18d78c293d477e0658e95
陈亮 <chenliang.info@gmail.com>
2015-01-09 15:06:26 +0800

    basic function hooked up

--- app.py
+++ app.py
@@ -2,13 +2,30 @@ __author__ = 'chenliang'
 
 #encoding = utf-8
 
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+import random
+
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
+#
+# type 1 for random character
+# type 2 for random poem line
+#
+@app.route('/random')
+def random_name():
+    return jsonify(type=1, v=unichr(random.randint(0x4000, 0x4999)))
+
+
+@app.route('/rank')
+def rank_name():
+    return jsonify(score=random.randint(0, 100), name=request.args.get('name', ''))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
