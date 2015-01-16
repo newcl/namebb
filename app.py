b6326904c4b26723ebbb879183de64d9d7518f22
陈亮 <chenliang.info@gmail.com>
2015-01-16 14:40:13 +0800

    ready or releasea

--- app.py
+++ app.py
@@ -4,10 +4,22 @@ __author__ = 'chenliang'
 
 from flask import Flask, jsonify, request, render_template
 import random
+import os
 
 
 app = Flask(__name__)
 app.debug = True
+
+boy_name_files = []
+girl_name_files = []
+
+boy_name_file_prefix = "split_boy"
+girl_name_file_prefix = "split_girl"
+
+def init_name_db():
+    for root, dirs, files in os.walk("./data"):
+        boy_name_files.extend(["./data/" + file for file in files if file.startswith(boy_name_file_prefix)])
+        girl_name_files.extend(["./data/" + file for file in files if file.startswith(girl_name_file_prefix)])
 
 @app.route('/')
 def index():
@@ -19,13 +31,35 @@ def index():
 #
 @app.route('/random')
 def random_name():
-    return jsonify(type=1, v=unichr(random.randint(0x4000, 0x4999)))
+    xing = request.args.get('xing', '')
+    word_count = request.args.get('wordCount', 0)
+    gender = request.args.get('gender', 'both')
+
+    name = ""
+    if (random.randint(0,2) == 0) :
+        name = random_boy_name();
+    else:
+        name = random_girl_name();
+    return jsonify(n=name)
 
 
 @app.route('/rank')
 def rank_name():
     return jsonify(score=random.randint(0, 100), name=request.args.get('name', ''))
 
+def random_boy_name():
+    return random_name(boy_name_files)
+
+def random_girl_name():
+    return random_name(girl_name_files)
+
+def random_name(files):
+    name_file_path = files[random.randint(0, len(files)-1)]
+    with open(name_file_path) as name_file:
+        names = [name for name in name_file.read().decode("utf-8").split("\n") if name.strip() != ""]
+        return names[random.randint(0, len(names))]
+
+init_name_db()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
